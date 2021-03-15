729dcd3ae98e86e81148f996083edcf975772c61
amarcs721992@gmail.com <amarcs721992@gmail.com>
2021-03-15 14:34:25 +0530

    CICD

--- src/main/java/com/abhishek/amar/OrderServiceApplication.java
+++ src/main/java/com/abhishek/amar/OrderServiceApplication.java
@@ -1,33 +1,15 @@
 package com.abhishek.amar;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/orders")
-public class OrderServiceApplication {
-
-    @Autowired
-    private OrderDao orderDao;
-
-    @GetMapping
-    public List<Order> fetchOrders() {
-        return orderDao.getOrders().stream().
-                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
-    }
+public class OrderServiceApplication { 
     
     @GetMapping("/demo")
 	String home() {
@@ -39,10 +21,7 @@ public class OrderServiceApplication {
 		System.out.println("Hii its demo !!!!!!");
 		return "Spring is here!  CICD application by demo1";
 	}
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> findById(@PathVariable("id")Integer id){
-    	return new ResponseEntity<Object>(orderDao.addOrders(id),HttpStatus.OK);
-    } 
+     
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
